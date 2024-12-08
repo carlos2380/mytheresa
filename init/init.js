2872214b924d47b16cc915ae4570ce32000b002e
Carlos <carlos.2380@hotmail.com>
2024-12-08 17:10:26 +0100

    Data and load data to mongodb

---
+++ init/init.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+db = db.getSiblingDB('mytheresadb');
+db.Product.createIndex({ category: 1 });
+db.Product.createIndex({ price: 1 });
+db.Product.createIndex({ category: 1, price: 1 });
+
+const fileProducts = JSON.parse(fs.readFileSync('/docker-entrypoint-initdb.d/products.json', 'utf8'));
+const products = fileProducts.products;
+
+const batchSize = 10000;
+for (let i = 0; i < products.length; i += batchSize) {
+    const batch = products.slice(i, i + batchSize);
+    db.Product.insertMany(batch);
+    print(`Insertado lote ${Math.ceil(i / batchSize) + 1} de ${Math.ceil(products.length / batchSize)}`);
+}
+
+db.Product.updateOne(
+    { sku: "000003" },
+    { $set: { sku_discount: 15 } }
+);
+
+db.DiscountCategory.createIndex({ category: 1 });
+
+const fileDiscounts = JSON.parse(fs.readFileSync('/docker-entrypoint-initdb.d/discounts.json', 'utf8'));
+const discounts = fileDiscounts.discounts_category;
+
+db.DiscountCategory.insertMany(discounts);
+
+print("Data succesfully load on mongodb.");
